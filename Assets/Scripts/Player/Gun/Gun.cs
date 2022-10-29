739d2e989d88f292e81fedfd3652e615b9a4c4eb
Vladyslav Kozlo <vladyslav.kozlo@gmail.com>
2022-10-29 20:55:35 +0300

    Fix animator

--- Assets/Scripts/Player/Gun/Gun.cs
+++ Assets/Scripts/Player/Gun/Gun.cs
@@ -37,7 +37,7 @@ namespace Player.Gun
             }
             if (Time.time >= nextTimeToFire)
             {
-                spawnPosition.localPosition = direction == Vector2.left ? new Vector3(-2, 0.5f, 0) : new Vector3(2, 0.5f, 0);
+                spawnPosition.localPosition = direction == Vector2.left ? new Vector3(-2.4f,0.6f,0) : new Vector3(2.4f,0.6f,0);
                 nextTimeToFire = Time.time + 1f / fireRate;
                 Bullet bullet = bullets.Dequeue();
                 bullet.transform.position = spawnPosition.position;
