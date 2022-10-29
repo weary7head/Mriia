f65aa30d1217b399c4115bd1ebfda5ca92c83bd4
Vanilex <67508081+Vanilex@users.noreply.github.com>
2022-10-29 14:42:34 +0300

    done

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset.x = 0;
+        offset.y = 0;
+        offset.z = -10;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f )
+        {
+            offset.z += Input.GetAxis("Mouse ScrollWheel") * 3;
+        }
+        
+        transform.position = target.position + offset;
+    }
+}
