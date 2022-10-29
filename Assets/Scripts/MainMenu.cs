84a6afeddd83f77a842306c133a54964aa7a4460
Yelyzaveta-Korolekh <liza.koroleh@ukr.net>
2022-10-29 22:42:33 +0300

    add MenuScene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ToSettings()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
